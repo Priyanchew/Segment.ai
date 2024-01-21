65e5465d93935c4deddeee4f352f276b11bf32f3
Priyanshu Choudhary <57816400+Priyanchew@users.noreply.github.com>
2024-01-21 09:21:23 +0530

    Add files via upload

---
+++ Segment/templates/myscripts.js
@@ -0,0 +1,8 @@
+var splashScreen = document.querySelector('.splash');
+splashScreen.addEventListener('click',()=>{
+  splashScreen.style.opacity = 0;
+  setTimeout(()=>{
+    splashScreen.classList.add('hidden')
+  },610)
+})
+ß
